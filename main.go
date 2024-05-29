662d27abc803fe86dfeaa28606dd2cf1e3c80662
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2024-05-30 00:58:46 +0330

    fix: changed the functions order in the rope file and removed main function junks cause added the tests :)

--- main.go
+++ main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/m-nt/gim/src/rope"
-)
-
 func main() {
-	fmt.Println("hey this is not a commit")
-	str := "hellow world"
-	the_rope := rope.Rope{}
-	the_rope.From_str(&str)
-	the_rope.Print()
-	// time.Sleep(time.Second)
-	the_rope.Insert(10, "another one")
-	fmt.Printf("%d\n", the_rope.Depth())
-	the_rope.Print()
-	// res := the_rope.Report()
-	// fmt.Printf("%s\n", *res)
-	the_rope.Insert(15, "ok")
-	fmt.Printf("%d\n", the_rope.Depth())
-	the_rope.Print()
-	// fmt.Printf("str: %s\n", *res2)
-	the_rope.Insert(13, "THIS IS WILD")
-	fmt.Printf("%d\n", the_rope.Depth())
-	the_rope.Print()
-	the_rope.Append("THIS IS APPENDED")
-	the_rope.Prepend("THIS IS PREPENDED")
-	// fmt.Printf("result: %+v\n", the_rope)
-	// time.Sleep(time.Second)
-	fmt.Printf("%d\n", the_rope.Depth())
-	// res2 := the_rope.Report()
-	// fmt.Printf("%s\n", *res2)
-	the_rope.Print()
+
 }
--- src/rope/rope.go
+++ src/rope/rope.go
@@ -19,10 +19,66 @@ func (rope *Rope) From_str(str *string) {
 	str_len := utf8.RuneCountInString(*str)
 	create_rope(rope, nil, str, 0, str_len-1)
 }
+
+func create_rope(rope *Rope, parent *Rope, str *string, L int, R int) {
+	rope.parent = parent
+	if (R - L) > LEAF_LEN {
+		rope.count = (R - L) / 2
+		mid := (L + R) / 2
+		rope.left = &Rope{}
+		rope.right = &Rope{}
+		create_rope(rope.left, rope, str, L, mid)
+		create_rope(rope.right, rope, str, mid+1, R)
+	} else {
+		rope.count = R - L
+		rope.str = (*str)[L : R+1]
+	}
+}
+
+func re_balance(rope *Rope) {
+	str := rope.ToString()
+	str_ln := utf8.RuneCountInString(*str)
+	create_rope(rope, nil, str, 0, str_ln-1)
+}
+
+func (rope *Rope) Insert(index int, value string) {
+	temp_rope := rope // this is the leaf that we merge the value with
+	t_index := index
+	for temp_rope.left != nil && temp_rope.right != nil {
+		if t_index >= temp_rope.count {
+			t_index = t_index - temp_rope.count - 1
+			temp_rope = temp_rope.right
+		} else {
+			temp_rope = temp_rope.left
+		}
+	}
+	new_data := temp_rope.str[:t_index] + value + temp_rope.str[t_index:]
+	str_ln := utf8.RuneCountInString(new_data)
+	temp_rope.str = ""
+	create_rope(temp_rope, temp_rope, &new_data, 0, str_ln-1)
+	re_balance(rope)
+}
+
+func (rope *Rope) Append(value string) {
+	str := rope.ToString()
+	str_ln := utf8.RuneCountInString(*str)
+	rope.Insert(str_ln, value)
+}
+
+func (rope *Rope) Prepend(value string) {
+	rope.Insert(0, value)
+}
+
+func (rope *Rope) Length() int {
+	str := rope.ToString()
+	return utf8.RuneCountInString(*str)
+}
+
 func (rope *Rope) Print() {
 	rope.print()
 	fmt.Printf("\n")
 }
+
 func (rope *Rope) print() {
 	if rope == nil {
 		return
@@ -47,51 +103,6 @@ func (rope *Rope) Depth() int {
 	}
 }
 
-func create_rope(rope *Rope, parent *Rope, str *string, L int, R int) {
-	rope.parent = parent
-	if (R - L) > LEAF_LEN {
-		rope.count = (R - L) / 2
-		mid := (L + R) / 2
-		rope.left = &Rope{}
-		rope.right = &Rope{}
-		create_rope(rope.left, rope, str, L, mid)
-		create_rope(rope.right, rope, str, mid+1, R)
-	} else {
-		rope.count = R - L
-		rope.str = (*str)[L : R+1]
-	}
-}
-
-func (rope *Rope) Insert(index int, value string) {
-	temp_rope := rope // this is the leaf that we merge the value with
-	t_index := index
-	for temp_rope.left != nil && temp_rope.right != nil {
-		if t_index >= temp_rope.count {
-			t_index = t_index - temp_rope.count - 1
-			temp_rope = temp_rope.right
-		} else {
-			temp_rope = temp_rope.left
-		}
-	}
-	new_data := temp_rope.str[:t_index] + value + temp_rope.str[t_index:]
-	str_ln := utf8.RuneCountInString(new_data)
-	temp_rope.str = ""
-	create_rope(temp_rope, temp_rope, &new_data, 0, str_ln-1)
-	re_balance(rope)
-}
-func (rope *Rope) Append(value string) {
-	str := rope.ToString()
-	str_ln := utf8.RuneCountInString(*str)
-	rope.Insert(str_ln, value)
-}
-func (rope *Rope) Prepend(value string) {
-	rope.Insert(0, value)
-}
-func (rope *Rope) Length() int {
-	str := rope.ToString()
-	return utf8.RuneCountInString(*str)
-}
-
 func (rope *Rope) ToString() *string {
 	if rope == nil {
 		return nil
@@ -102,10 +113,4 @@ func (rope *Rope) ToString() *string {
 	right_str := *rope.right.ToString()
 	res := left_str + right_str
 	return &res
-}
-
-func re_balance(rope *Rope) {
-	str := rope.ToString()
-	str_ln := utf8.RuneCountInString(*str)
-	create_rope(rope, nil, str, 0, str_ln-1)
 }
